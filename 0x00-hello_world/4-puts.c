09458a975a44e223a58b200e40d041e44c759b79
Robel Wondimu <98416443+Robaplus75@users.noreply.github.com>
2023-03-18 00:00:23 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * puts  programming is like building a multilingual puzzle
+ *
+ * and returns 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

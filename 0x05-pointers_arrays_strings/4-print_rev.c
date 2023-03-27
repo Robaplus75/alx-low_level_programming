c97587bf74ac33f340c4f507ba258b71473f25f3
Robel <robelwon75@gmail.com>
2023-03-27 18:24:31 +0300

    reverser

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_rev - reverser
+ * @s: string to be reversed
+ * Return: the reversed string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i != -1)
+	{
+		putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}

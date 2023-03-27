560a2bebf7188a9164de35b7c9daf282ed527481
Robel <robelwon75@gmail.com>
2023-03-27 17:45:27 +0300

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets to 98
+ * @n: the number
+ * Return: none
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,2 @@
 void swap_int(int *m, int *j);
+void reset_to_98(int *n);

cfd65b52637c1a6db3c4961daf27ae55957678b1
Robel <robelwon75@gmail.com>
2023-04-04 22:16:01 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,35 +11,20 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	int k = 0;
-	char word[100];
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		while (s[i] != c)
 		{
-			int j = 0;
-
-			k++;
-			while (s[i] != '\0')
-			{
-				*(word + j) = s[i];
-				j++;
-			}
-			break;
+			i++;
 		}
-		i++;
 	}
-	if (k == 0)
+	if (s[i] == c)
 	{
-		return (NULL);
+		return (&s[i]);
 	}
 	else
 	{
-		for (int m = 0; word[m] != '\0'; m++)
-		{
-			*(s + m) = *(word + m);
-		}
-		return (s);
+		return (NULL);
 	}
 }

35425db1952e30b420c5eacf55707805267a7b8d
Robel <robelwon75@gmail.com>
2023-04-04 22:27:58 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,9 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	for (int k = 0; s[k] != '\0' && s[k] != c; k++)
 	{
-		while (s[i] != c)
-		{
-			i++;
-		}
+		i++;
 	}
 	if (s[i] == c)
 	{

5cbf7d79445255e72c0ad5b443de87fcc67dd594
Robel <robelwon75@gmail.com>
2023-04-04 19:59:13 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - replaces the first n elements by b
+ * @s: string to be replced or filled
+ * @b: characher to use
+ * @n: number of items to be replaced
+ *
+ * Return: the replaced string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (n > 0)
+	{
+		*(s + i) = b;
+		i++;
+		n--;
+	}
+	return (s);
+}

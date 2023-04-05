92ca831c365e4c9769819cac698834600594bd5f
Robel <robelwon75@gmail.com>
2023-04-05 13:35:59 +0300

    puts_recursiton

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ *
+ * Return: none
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	putchar('\n');
+}

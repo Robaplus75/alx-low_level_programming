e17604a165193c45291f24d669b151fc35a5ab6f
Robel <robelwon75@gmail.com>
2023-04-05 14:48:15 +0300

    counter

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - string length counder
+ * @s: the string
+ *
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}

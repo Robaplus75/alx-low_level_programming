2ea849459f5599dd1d3c7ad49e294d3724d6408d
Robel <robelwon75@gmail.com>
2023-04-07 23:57:16 +0300

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: argument
+ * Return: none
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

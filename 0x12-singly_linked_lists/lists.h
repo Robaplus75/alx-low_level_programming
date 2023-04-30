bbb2dc4aa69ff41fc9db222d3565e756c1af4158
Robel <robelwon75@gmail.com>
2023-04-30 12:47:43 +0300

    lists.h betty fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 list_t *add_node(list_t **head, const char *str);
